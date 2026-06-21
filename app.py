547cdcb30935cbe9ac432a8d658d1230ae95bb0a
nguyenphuonganhhhhhhh-tech <nguyenphuonganhhhhhhh@gmail.com>
2026-06-21 21:28:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -390,7 +390,7 @@ STATIONS = [
         "name": "Trạm 47",
         "image": "47.png",
         "answers": {
-            "1": "nếp vị", "2": "lỗ tâm vị", "3": "cơ thắt môn vị", "4": "lớp thanh mạc"
+            "1": "nếp vị", "2": "lỗ tâm vị", "3": "cơ thắt môn vị", "4": "lớp thanh mạc", "5": "lỗ môn vị"
         }
     },
 ]
